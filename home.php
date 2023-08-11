fdb484b81f493af767caecee69d45d4ac58c8770
ARJUN <arjun246t@gmail.com>
2023-08-11 12:44:00 +0530

    css

---
+++ home.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+
+    <form action="" method="post">
+        <div class="nav_links">
+            <div class="logo">
+                <a href="home.php"><img src="logo2.png"  alt="#"></a>
+                
+            </div>
+            
+            <ul>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a id=button href="indexreg.php">Sign Up</a></li>
+            
+            </ul>
+        </div>
+
+        <div class="support">
+            <p>Popular Brands We Support</p>
+        </div>
+        
+        <div class="brands">
+            <div class="images">
+                <img class="img1" src="samsung.png" alt="">
+                <img class="img2" src="apple.png" alt="">
+                <img class="img3" src="mi.png" alt="">
+                <img class="img4" src="oneplus.png" alt="">
+            </div>
+        </div>
+        
+    <div class="content">
+            
+            <h3>The Repair Process</h3>
+            <p>We Make Sure That Every Customer is satisfied with our Service.</p>
+            <p>We Provide Fast & Reliable Repair Charges.</p>
+            <p>Customers Responses Are Our First Priority.</p>
+    </div>
+
+    
+     
+        
+
+    </form>
+    
+
+    
+
+   
+</body>
+</html>
